a2213312c86b9c13d1544dbd296d3bde9ccd6c7b
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2022-08-02 00:18:24 +0200

    [test] sqlite bun api

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
 import { Hono } from "hono";
+import { Database } from "bun:sqlite";
 
+interface Payload {
+  fkbr: string
+}
+
+// init database
+const database = new Database("dog.sqlite", { readwrite: true, create: true })
+
+// TODO: migrations
+database.run("CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT NOT NULL);")
+
+// init hono app
 const app = new Hono();
-
 const port = parseInt(process.env.PORT) || 3000;
 
+// Routing
 const home = app.get("/", (c) => {
   return c.json({ message: "Hello World!" });
 });
+
+app.get('/test', (c) => {
+  return c.json(database.query("SELECT data FROM test").all())
+})
+
+app.post('/test', async (c) => {
+  const json = await c.req.json<Payload>();
+
+  database.run("INSERT INTO test(data) VALUES (?)", json.fkbr);
+
+  return c.json({ yolo: true })
+})
 
 export default {
   port,
